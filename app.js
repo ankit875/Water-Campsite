c4e0bb3b05492fdd5d324466d37decf61c7b111d
Ankit <Ankitagrawal620@live.com>
2018-07-25 08:45:50 +0530

    initial deploy commit

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use( "/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/",authRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server has Started!!");
 })
